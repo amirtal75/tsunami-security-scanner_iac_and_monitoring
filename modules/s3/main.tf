a03b0eb01c80adc3d36ced895dda7ae7e84fa10a
Amir Tal <amir_tal@outlook.com>
2024-07-18 10:59:05 +0300

    add module for s3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -26,9 +26,17 @@ resource "aws_s3_bucket_policy" "this" {
   })
 }
 
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.this.id
   acl    = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.this]
 }
 
 resource "aws_s3_bucket_versioning" "this" {
