a455ddac15a76e0fabb8b0240fb1c29d7e92d635
Amir Tal <amir_tal@outlook.com>
2024-07-21 00:02:55 +0300

    fix missing arg for scan

--- tsunami_build/poll_and_scan.py
+++ tsunami_build/poll_and_scan.py
@@ -45,7 +45,7 @@ def run_tsunami_scan(ip):
     try:
         result = subprocess.run(
             ["java", "-cp", "tsunami.jar:plugins/*", "-Dtsunami-config.location=tsunami.yaml",
-             "com.google.tsunami.main.cli.TsunamiCli", '--ip-v4-target'],
+             "com.google.tsunami.main.cli.TsunamiCli", '--ip-v4-target', ip],
             capture_output=True,
             text=True
         )
